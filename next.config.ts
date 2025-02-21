afff680ccec9924849f7821f91a6dd595b408f2b
Souleymane DIALLO <sdley2020@gmail.com>
2025-02-21 22:13:36 +0000

    Avatar Component

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["m.media-amazon.com"],
+    domains: ["m.media-amazon.com", "lh3.googleusercontent.com"],
   },
 };
 
