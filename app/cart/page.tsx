837629dcf381295baa74ebf998ee544039186995
Souleymane DIALLO <sdley2020@gmail.com>
2025-02-22 18:04:57 +0000

    Cart UI

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,12 @@
+import Container from "../components/Container";
+import CartClient from "./CartClient";
+
 const Cart = () => {
     return ( 
-        <div>
-            <h1>Cart Page</h1>
+        <div className="pt-8">
+            <Container>
+                <CartClient />
+            </Container>
         </div>
      );
 }
