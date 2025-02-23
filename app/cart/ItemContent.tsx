5ebb85ccf162bb65e2419fe5b838909db70c7d2a
Souleymane DIALLO <sdley2020@gmail.com>
2025-02-23 13:01:45 +0000

    Cart Mobile Responsiveness V2

--- app/cart/ItemContent.tsx
+++ app/cart/ItemContent.tsx
@@ -1,4 +1,4 @@
-'use client'; 
+'use client';
 
 import { formatPrice } from "@/utils/FormatPrice";
 import { CartProductType } from "../product/[productId]/ProductDetails";
@@ -19,31 +19,40 @@ const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
         handleCartQtyDecrease
     } = useCart();
 
-
     return ( 
         <div
             className="
-            flex
-            flex-col
-            md:grid
-            grid-cols-5
-            text-xs
-            md:text-sm
-            gap-4
-            border-t-[1.5px]
-            border-slate-200
-            py-4
-            items-center
+                flex
+                flex-col
+                md:grid
+                md:grid-cols-5
+                text-xs
+                md:text-sm
+                gap-4
+                border-t-[1.5px]
+                border-slate-200
+                py-4
+                items-center
             "
         >
+            {/* Product Info Section */}
             <div 
-            className="
-                col-span-2
-                justify-self-start
-                flex
-                gap-2
-                md:gap-4
-            ">
+                // className="
+                //     flex
+                //     items-center
+                //     gap-2
+                //     md:col-span-2
+                //     w-full
+                // "
+                className="
+                    flex
+                    gap-2 
+                    justify-self-start
+                    md:col-span-2 
+                    md:gap-4
+                    w-full
+                "
+            >
                 <Link href={`/product/${item.id}`}>
                     <div className="relative w-[70px] aspect-square">
                         <Image 
@@ -54,34 +63,57 @@ const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
                         />
                     </div>
                 </Link>
-                <div
-                className="
-                    flex
-                    flex-col
-                    justify-between
-                "
+                <div 
+                // className="flex flex-col justify-center items-center"
+                className="flex flex-col items-start justify-between"
+                
                 >
                     <Link href={`/product/${item.id}`}>
                         {truncateText(item.name)}
                     </Link>
                     <div>{item.selectedImg.color}</div>
-                    <div className="w-[70px]">
-                        <button className="text-slate-500 underline" onClick={() => handleRemoveProductFromCart(item)}>Remove</button>
-                    </div>
+                    <button 
+                        className="text-slate-500 underline"
+                        onClick={() => handleRemoveProductFromCart(item)}
+                    >
+                        Remove
+                    </button>
                 </div>
             </div>
-            <div className="justify-self-center">{formatPrice(item.price)}</div>
-            <div className="justify-self-center">
+
+            {/* Price Section */}
+            <div 
+                className="
+                    flex justify-between items-center w-full
+                    md:justify-self-center md:flex-none md:w-auto">
+                <span className="md:hidden font-semibold uppercase">Price:</span>
+                <span>{formatPrice(item.price)}</span>
+            </div>
+
+            {/* Quantity Section */}
+            <div 
+                className="
+                    flex justify-between items-center w-full
+                    md:justify-self-center md:flex-none md:w-auto">
+                <span className="md:hidden font-semibold uppercase">Quantity:</span>
                 <SetQuantity 
                     cartCounter={true}
                     cartProduct={item}
-                    handleQtyIncrease={() => {handleCartQtyIncrease(item)}}
-                    handleQtyDecrease={() => {handleCartQtyDecrease(item)}}
+                    handleQtyIncrease={() => handleCartQtyIncrease(item)}
+                    handleQtyDecrease={() => handleCartQtyDecrease(item)}
                 />
             </div>
-            <div className="justify-self-end font-semibold">{formatPrice(item.price * item.quantity)}</div>
+
+            {/* Total Section */}
+            <div 
+                className="
+                    flex justify-between items-center w-full font-semibold
+                    md:justify-self-end md:flex-none md:w-auto">
+                <span className="md:hidden font-semibold uppercase">Total:</span>
+                <span>{formatPrice(item.price * item.quantity)}</span>
+            </div>
         </div>
-     );
+    );
 }
  
 export default ItemContent;
