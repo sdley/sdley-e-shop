210664b23fa5ae1f5d5b39d579cb9a2254a3a727
Souleymane DIALLO <sdley2020@gmail.com>
2025-02-24 21:39:22 +0000

    await params

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -7,8 +7,9 @@ interface IParams {
     productId: string;
 }
 
-const Product = ({ params }: {params: IParams}) => {
-    console.log("params", params); // Server Side Component => Check the Server Console to view it 
+const Product = async (props: {params: Promise<IParams>}) => {
+    const params = await props.params;
+    // console.log("params", params); // Server Side Component => Check the Server Console to view it 
     // but not the browser console!
 
     const product = products.find((item) => item.id === params.productId);
