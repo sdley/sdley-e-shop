4c342ed7b4aac81965bacd9b038518195c4eab2e
Souleymane DIALLO <sdley2020@gmail.com>
2025-02-24 10:16:21 +0000

    User Menu

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Container from "../Container";
 import { Redressed } from "next/font/google";
 import CartCount from "./CartCount";
+import UserMenu from "./UserMenu";
 
 const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
@@ -33,7 +34,7 @@ const NavBar = () => {
                         <div className="block max-md:hidden">Search</div>
                         <div className="flex items-center gap-8 md:gap-12">
                             <CartCount />
-                            <div>UserMenu</div>
+                            <UserMenu />
                         </div>
                     </div>
                 </Container>
