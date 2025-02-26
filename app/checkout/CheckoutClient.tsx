0ae4c2c20fca692f044a4a04510d241b848073ed
Souleymane DIALLO <sdley2020@gmail.com>
2025-02-26 16:43:11 +0000

    Stripe Form Elements

--- app/checkout/CheckoutClient.tsx
+++ app/checkout/CheckoutClient.tsx
@@ -2,14 +2,21 @@
 
 import { useCart } from "@/hooks/useCart";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import Button from "../components/Button";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string);
 
 const CheckoutClient = () => {
     const { cartProducts, paymentIntent, handleSetPaymentIntent } = useCart();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [clientSecret, setClientSecret] = useState('');
+    const [paymentSuccess, setPaymentSuccess] = useState(false);
 
     const router = useRouter();
 
@@ -46,12 +53,43 @@ const CheckoutClient = () => {
             })
         }
 
-    }, [cartProducts, paymentIntent])
+    }, [cartProducts, paymentIntent, handleSetPaymentIntent, router]);
+
+    const options: StripeElementsOptions = {
+        clientSecret,
+        appearance: {
+            theme: 'stripe',
+            labels: 'floating',
+        },
+    };
+
+    const handleSetPaymentSuccess = useCallback((value: boolean) => {
+        setPaymentSuccess(value);
+    }, []);
 
     return ( 
-        <>
-            Checkout
-        </>
+        <div className="w-full">
+            {clientSecret && cartProducts && (
+                <Elements options={options} stripe={stripePromise}>
+                    <CheckoutForm clientSecret={clientSecret} handleSetPaymentSuccess={handleSetPaymentSuccess} />
+                </Elements>
+            )}
+            {loading && <div className="text-center">Loading Checkout...</div>}
+            {error && (
+                <div className="text-center text-rose-500">Something went wrong...</div>
+            )}
+            {paymentSuccess && (
+                <div className="flex items-center flex-col gap-4">
+                    <div className="text-teal-500 text-center">Payment Success!</div>
+                    <div className="max-w-[220px] w-full">
+                        <Button
+                            label="View Your Orders"
+                            onClick={() => router.push('/order')}
+                        />
+                    </div>
+                </div>
+            )}
+        </div>
      );
 }
  
---
+++ app/checkout/CheckoutForm.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { useCart } from "@/hooks/useCart";
+import { formatPrice } from "@/utils/FormatPrice";
+import { AddressElement, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import Heading from "../components/Heading";
+import Button from "../components/Button";
+
+interface CheckoutFormProps {
+    clientSecret: string;
+    handleSetPaymentSuccess: (value: boolean) => void;
+}
+
+const CheckoutForm: React.FC<CheckoutFormProps> = ({
+    clientSecret,
+    handleSetPaymentSuccess,
+}) => {
+    const {cartTotalAmount, handleClearCart, handleSetPaymentIntent} = useCart();
+    const stripe = useStripe();
+    const elements = useElements();
+    const [isLoading, setIsLoading] = useState(false);
+    const formattedPrice = formatPrice(cartTotalAmount);
+
+    useEffect(() => {
+        if (!stripe) return;
+
+        if (!clientSecret) return;
+
+        handleSetPaymentSuccess(false);
+    }, [stripe, clientSecret, handleSetPaymentSuccess]);
+
+    const handleSubmit = async(e: React.FormEvent) => {
+        // prevent the form from submitting | refreshing
+        e.preventDefault();
+
+        if (!stripe || !elements) return;
+
+        setIsLoading(true);
+
+        stripe.confirmPayment({
+            // payments data to be sent to stripe
+            elements, 
+            redirect: 'if_required'
+        }).then(result => {
+            if (!result.error){
+                toast.success('Payment successful');
+
+                handleClearCart();
+                handleSetPaymentSuccess(true);
+                handleSetPaymentIntent(null);
+            }
+
+            setIsLoading(false);
+        });
+    }
+
+    return ( 
+        <form onSubmit={handleSubmit} id="payment-form">
+            <div className="mb-6">
+                <Heading title="Enter your details to complete checkout" />
+            </div>
+            <h2 className="font-semibold mb-2">
+                Address Information
+            </h2>
+            <AddressElement 
+                options={{
+                    mode: 'shipping',
+                    allowedCountries: ['SN', 'CI', 'GN', 'ML', 'BF']
+                }}
+            />
+            <h2 className="font-semibold mt-4 mb-2">Payment Information</h2>
+            <PaymentElement id="payment-element" options={{layout: 'auto'}} />
+            <div className="py-4 text-center text-slate-700 text-xl font-bold">
+                Total: {formattedPrice}
+            </div>
+            <Button
+                label={isLoading ? 'Processing Payment...' : 'Pay Now'}
+                disabled={isLoading || !stripe || !elements}
+                onClick={() => {}}
+            />
+        </form>
+     );
+}
+ 
+export default CheckoutForm;
